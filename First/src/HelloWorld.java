d19fab9a26b03f772efcd0cd63f7bb209dda746c
AlexCoroiu <Alex@DESKTOP-PP8CC1J>
2016-12-06 21:13:18 +0100

    method

--- First/src/HelloWorld.java
+++ First/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	void print(){
 		System.out.println("Hello , world!!!");
 	}
+	void method(){}
 	public static void main(String[] args){
 		HelloWorld message= new HelloWorld();
 		message.print();
